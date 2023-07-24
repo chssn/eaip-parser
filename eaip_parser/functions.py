2c74fa032fdd7a46c58cb22c316ab120439bef1f
chssn <33300267+chssn@users.noreply.github.com>
2023-07-24 15:04:24 +0100

    fix: improved error checking

--- eaip_parser/functions.py
+++ eaip_parser/functions.py
@@ -18,31 +18,39 @@ class Geo:
     """Class containing various geo tools"""
 
     @staticmethod
-    def north_south(arg):
+    def north_south(arg:str) -> str:
         """Turns a compass point into the correct + or - for lat and long"""
-        if arg == '+':
-            return "N"
-        return "S"
+        if arg in ("+","-"):
+            if arg == '+':
+                return "N"
+            return "S"
+        raise ValueError("This function only accepts a '+' or '-' as an input!")
 
     @staticmethod
-    def east_west(arg):
+    def east_west(arg:str) -> str:
         """Turns a compass point into the correct + or - for lat and long"""
-        if arg == '+':
-            return "E"
-        return "W"
+        if arg in ("+","-"):
+            if arg == '+':
+                return "E"
+            return "W"
+        raise ValueError("This function only accepts a '+' or '-' as an input!")
 
     @staticmethod
-    def plus_minus(arg):
+    def plus_minus(arg:str) -> str:
         """Turns a compass point into the correct + or - for lat and long"""
-        if arg in ('N','E'):
-            return "+"
-        return "-"
+        if arg in ("N","E","S","W"):
+            if arg in ('N','E'):
+                return "+"
+            return "-"
+        raise ValueError("This function only accepts a 'N', 'E', 'S' or 'W' as an input!")
 
     @staticmethod
-    def back_bearing(brg):
+    def back_bearing(brg:float) -> float:
         """Calculates a back bearing"""
-        if (float(brg) - 180) < 0:
-            back = float(brg) + 180.00
-        else:
-            back = float(brg) - 180.00
-        return round(back, 2)
+        if float(brg) <= 360 and float(brg) >=0:
+            if (float(brg) - 180) < 0:
+                back = float(brg) + 180
+            else:
+                back = float(brg) - 180
+            return round(back, 2)
+        raise ValueError("This function only accepts an input between 0 and 360")
